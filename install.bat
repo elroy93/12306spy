a0e5c7a8c226ec953a13fdee58408bbcf05fe56f
elroy <elroyfunlife@gmail.com>
2024-02-06 19:00:33 +0800

    install.bat

--- install.bat
+++ install.bat
@@ -1,30 +1,54 @@
 @echo off
 chcp 65001
 
-echo Creating virtual environment...
 
+:: 检查python环境, 如果没有安装python, 请先安装python
+python --version > nul 2>&1
+if %errorlevel% neq 0 (
+    powershell -command "Write-Host 'Python is not installed. Please install Python 3.7 or above.' -ForegroundColor Red"
+    echo downloads from  :  https://www.python.org/downloads/
+    pause
+    exit /b
+)
+for /f "tokens=2 delims=." %%v in ('python --version 2^>^&1') do (
+    set /a version=%%v
+)
+
+if %version% lss 7 (
+    powershell -command "Write-Host 'Python version is below 3.7. Please install Python 3.7 or above' -ForegroundColor Red"
+    echo downloads from  :  https://www.python.org/downloads/
+    pause
+    exit /b
+)
+echo Python is installed and meets the minimum version requirement.
+
+:: 检查结束
+python --version
+
+powershell -command "Write-Host 'create virtual environment... ' -ForegroundColor Green"
 python -m venv venv
-echo Activating virtual environment...
+
+powershell -command "Write-Host 'activating virtual environment... ' -ForegroundColor Green"
 call .\venv\Scripts\activate.bat
 
-
 :: 检查当前的虚拟环境
-echo %cd%
+echo current path : %cd%
 
-echo Installing dependencies from requirements.txt...
+echo installing dependencies from requirements.txt...
 :: 更新pip
 python -m pip install --upgrade pip
 pip install -r requirements.txt
 
-echo --------------------------
-echo install success 
-echo --------------------------
-
+powershell -command "Write-Host '--------------------------' -ForegroundColor Green"
+powershell -command "Write-Host '     install success      ' -ForegroundColor Green"
+powershell -command "Write-Host '--------------------------' -ForegroundColor Green"
 
 :: 用jupyter notebook打开 main.ipynb
+
+powershell -command "Write-Host 'start jupyter notebook' -ForegroundColor Green"
 jupyter notebook main.ipynb
 
 pause
-
 :: 退出虚拟环境
 deactivate
+exit /b
