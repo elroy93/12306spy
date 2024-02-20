6d8085df0b656d811e873c3fde2afac29f53ad73
elroy <elroyfunlife@gmail.com>
2024-02-20 21:57:32 +0800

    add docker

--- stop_docker.sh
+++ stop_docker.sh
@@ -1,7 +1,3 @@
 
 # 停止IMAGE  名字叫做12306spy的容器
 docker stop $(docker ps -a | grep 12306spy | awk '{print $1}')
-
-docker run -d -p 8081:8080 12306spy
-
-# registry.cn-hangzhou.aliyuncs.com/elroy93/12306spy
--- test.py
+++
@@ -1,9 +0,0 @@
-from orjson import orjson
-
-with open("data.json", "r", encoding="utf-8") as f:
-    data = f.read()
-    orjson.loads(data)
-    print(orjson.loads(data))
-
-for i in range(0, 10):
-    print(i)
